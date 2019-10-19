c9ed9b341b3b89e36a063d36a9be7cacf0b3cf7b
mystralkk <mystralkk@gmail.com>
2019-10-19 16:58:34 +0900

    Added Japanese language file

---
+++ language/japanese_utf-8.php
@@ -0,0 +1,197 @@
+<?php
+
+/* Reminder: always indent with 4 spaces (no tabs). */
+// +-----------------------------------------------------------------------------+
+// | glMessenger Plugin 1.0 for Geeklog- The Ultimate OSS Portal                 |
+// | Date: November 15, 2003                                                     |
+// +-----------------------------------------------------------------------------+
+// | english.php - English Language Filem                                        |
+// +-----------------------------------------------------------------------------+
+// | Copyright (C) 2003-2019 by the following authors:                           |
+// |                                                                             |
+// | glMessenger Plugin Author:                                                  |
+// | Blaine Lang   -  blaine@portalparts.com                                     |
+// | Kenji ITO     -  mystralkk AT gmail DOT com                                 |
+// +-----------------------------------------------------------------------------+
+// | Geeklog Common Code                                                         |
+// | Copyright (C) 2000-2003 by the following authors:                           |
+// |                                                                             |
+// | Authors: Tony Bibbs        - tony@tonybibbs.com                             |
+// |          Mark Limburg      - mlimburg@users.sourceforge.net                 |
+// |          Jason Whittenburg - jwhitten@securitygeeks.com                     |
+// |          Dirk Haun         - dirk@haun-online.de                            |
+// +-----------------------------------------------------------------------------+
+// |                                                                             |
+// | This program is licensed under the terms of the GNU General Public License  |
+// | as published by the Free Software Foundation; either version 2              |
+// | of the License, or (at your option) any later version.                      |
+// |                                                                             |
+// | This program is OpenSource but not FREE. Unauthorized distribution is       |
+// | illegal. Anyone caught doing so will be banished from the geeklog community |
+// | You may not remove the copyright or redistribute this script in any form.   |
+// |                                                                             |
+// | This program is distributed in the hope that it will be useful,             |
+// | but WITHOUT ANY WARRANTY; without even the implied warranty of              |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.                        |
+// | See the GNU General Public License for more details.                        |
+// |                                                                             |
+// | You should have received a copy of the GNU General Public License           |
+// | along with this program; if not, write to the Free Software Foundation,     |
+// | Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             |
+// |                                                                             |
+// +-----------------------------------------------------------------------------+
+//
+
+$LANG_MSG00 = array (
+    'usermenu'          => 'プライベートメッセージ',
+    'plugin'            => 'プラグイン',
+    'access_denied'     => 'アクセスが拒否されました。',
+    'access_denied_msg' => 'このページにアクセスできるのはRootユーザーだけです。あなたのユーザー名とIPアドレスは記録されました。',
+    'admin'             => 'プラグイン管理者',
+    'install_header'    => 'プラグインのインストール/アンインストール',
+    'installed'         => 'glMessengerプラグインとSmilieエディターがインストールされました。<p/>次のステップ:<br>1) インストールの注意書きに従ってテーマとCSSを更新する<br>2) 通知のログをとるには notification.log を作成する<p><i>楽しんでください,<br><a href="MAILTO:langmail@sympatico.ca">Blaine</a></i>',
+    'uninstalled'       => 'プラグインがインストールされていません',
+    'install_success'   => 'インストール成功', 
+    'install_failed'    => 'インストール失敗 -- 詳細は error.log を参照してください。',
+    'uninstall_msg'     => 'プラグインをアンインストールしました。',
+    'install'           => 'インストール',
+    'uninstall'         => 'アンインストール',
+    'enabled'           => '<br>プラグインがインストールされ有効になっています。<br>アンインストールする前にまず無効にしてください。<p>',
+    'warning'           => 'Messengerプラグインアンインストール警告',
+    'editor'            => 'プラグインエディターへ戻る',
+
+);
+
+$LANG_MSG  = array(
+    'err01'             => 'この機能へのアクセス権がありません',
+    'err02'             => '不正な操作を実行しようとしました',
+    'err03'             => 'エラー: メンバー名が無効です',
+    'BLOCKHEADER'       => 'プライベートメッセージ',
+    'RE'                => 'Re:',
+    'ERROR'             => 'エラー!',
+    'FROM'              => '差出人',
+    'TO'                => '宛先',
+    'SUBJECT'           => '件名',
+    'PM'                => 'PM',
+    'NEW_PM'            => '新しいPM',
+    'NEW'               => 'New',
+    'NEWMESSAGE'        => 'メッセージ作成',
+    'DATE'              => '日付',
+    'ARCHIVE'           => '&nbsp;保存&nbsp;',
+    'ARCHIVEMSG'        => 'メッセージを保存',
+    'REPLY'             => '&nbsp;返信&nbsp;',
+    'REPLYMSG'          => '差出人へ返信',
+    'DELETE'            => '&nbsp;削除&nbsp;',
+    'DELETEMSG'         => 'メッセージを削除',
+    'INBOX'             => '受信箱',
+    'OUTBOX'            => '送信箱',
+    'SENTBOX'           => '送信済み',
+    'ARCHIVEBOX'        => '保存',
+    'REMOVE'            => 'このメッセージを削除する',
+    'SUBMIT'            => '送信',
+    'PREVIEW'           => 'プレビュー',
+    'MESSAGE'           => 'メッセージ',
+    'MEMBERS'           => 'メンバー',
+    'MEMBERSLIST'       => 'メンバーリスト',
+    'ANONYMOUS'         => 'ゲスト',
+    'BROADCAST'         => '一括送信',
+    'BROADCAST_MSG'     => 'メッセージを一括送信する',
+    'EXPIRE_MSG'        => 'メッセージは %s に削除されます',
+    'OPTIONS'           => 'オプション',
+    'DELALL'            => 'すべて削除',
+    'DELALLMSG'         => 'すべてのメッセージを削除する',
+    'DELOLDER'          => '古いメッセージの削除',
+    'DELOLDERMSG'       => '昨日までのすべてのメッセージを削除する',
+    'VIEWORIGINAL'      => '<hr>表示&nbsp;<a href="%s">元の </a>&nbsp;メッセージ',
+    'MYBUDDIES'         => 'お気に入りメンバー',
+    'MSGFAVORITES'      => 'Messenger Favorites',
+    'ADDBUDDY'          => 'メンバーを追加する',
+    'DELBUDDY'          => 'メンバーを削除する',
+    'HOME'              => 'ホーム',
+    'prompt01'          => 'このページは自動的に更新されません。更新するには<a href=%s>ここ</a>をクリックしてください',
+    'prompt02'          => '* 一括送信メッセージ。送信者か管理者しか削除できません。<br>&nbsp;削除するとコピーもすべて削除されます。',
+    'prompt02b'         => '* 一括送信メッセージ。送信者か管理者しか削除できません。',
+    'prompt03'          => '送信日時: ',
+    'statusmsg1'        => '%s 続けるには<a href="%s">ここ</a>をクリック',
+    'statusmsg2'        => 'to continue',
+    'statusmsg3'        => 'このページは自動的に戻ります。すぐに戻るには<a href="%s">ここ</a>をクリックしてください。',
+    'msgsent'           => 'メッセージを送信しました。',
+    'msgsave'           => 'メッセージが %s へ送信されました。',
+    'msgreturn'         => 'to return to your inbox.',
+    'msgerror'          => 'メッセージが送信されませんでした。<a href="javascript:history.back()">ここをクリック</a>して、すべてのフィールドに入力したか確認してください。',
+    'msgdelok'          => '削除しました',
+    'msgdelsuccess'     => 'メッセージを削除しました。',
+    'msgdelerr'         => 'メッセージを削除しませんでした。<a href=\"javascript:history.back()\">ここをクリック</a>して、メッセージを選択してください',
+    'msgpriv'           => 'プライベートメッセージ',
+    'msgprivnote1'      => 'You have %s private message in your %s folder.',
+    'msgprivnote2'      => 'You have %s private messages in your %s folder.',
+    'msgto'             => '宛先: ',
+    'msgmembers'        => 'メンバーリスト',
+    'msgarchive'        => 'メッセージを保存しました。',
+    'newmsghelp'        => 'メッセージの新規作成',
+    'lang_broadcasts'   => 'PMの一括送信',
+    'help_broadcasts'   => 'メッセンジャーの一括送信をブロック',
+    'lang_notifications'       => '通知',
+    'help_notifications'       => '新しいメッセージや購読しているメッセージの更新があった際にメールで通知',
+    'lang_sitenotifications'   => 'メッセンジャーをデフォルトにする',
+    'help_sitenotifications'   => 'すべての通知をプライベートメッセージの受信箱に送る',
+    'outboxmsg'         => '受信者がまだ削除していないため、送信箱のメッセージを削除できません。'
+);
+
+
+// Language used for Notification Feature
+$LANG_MSG01 = array (
+    'HELLO'          => "Hello",
+    'ADMIN'          => "Site Administrator",
+    'SUBJECT'        => "- New Private Message Notifiction",
+    'BROADCAST'      => "- Broadcast Message Notifiction",
+    'LINE1'          => "You have a new Private message from fellow site member: %s\nSubject:%s",
+    'LINE1B'         => "There is a new broadcast message waiting in your private message area from: %s\nSubject: %s",
+    'LINE2'          => "\n \nYou are receiving this because you requested to be notified of any new Private Messages.\n\n",
+    'LINE3'          => "Have a great day!\n"
+);
+
+// Language used for Admin screens
+$LANG_MSG02 = array (
+    'BLOCKHEADER'     => "スマイリー管理",
+    'IMAGE'           => "画像",
+    'CODE'            => "絵文字",
+    'DESCRIPTION'     => "説明",
+    'ADDSMILIE'       => "スマイリーを追加",
+    'EDIT'            => "編集",
+    'DELETE'          => "削除",
+    'ADDPROMPT'       => "新しいスマイリーを追加",
+    'ADDSUBMIT'       => "新しいスマイリーを追加",
+    'EDITPROMPT'      => "スマイリーの編集",
+    'EDITSUBMIT'      => "スマイリーの更新",
+    'FILENAME'        => "画像ファイル名",
+    'EDIT'            => "編集",
+    'HELPMSG1'        => 'この画面でスマイリーの追加・編集・削除を行います。同じスマイリーの画像に対して複数の顔文字を設定することができます。スマイリーを追加するには、画像ファイルをサーバーのスマイリーを保存しているディレクトリにアップロードしてから「スマイリーを追加」をクリックしてください。顔文字や割り当てる画像を変更するには「編集」をクリックしてください。'
+);
+
+// Language used for Sortby Filter in main message view
+$LANG_MSG03 = array (
+    'BUDDYADMIN'  => "メンバー管理",
+    'SORTBY'      => "ソート基準",
+    'OLDFIRST'    => "古い順",
+    'NEWFIRST'    => "新しい順",
+    'MEMBER'      => "メンバー名"
+);
+
+// Language used in the Buddy Admin screen
+$LANG_MSG04 = array (
+    'MAINHEADER'      => "メンバー管理",
+    'HEADER1'         => "追加可能なメンバー",
+    'HEADER2'         => "メンバーリスト",
+    'ADD'             => "追加",
+    'REMOVE'          => "削除",
+    'CANCEL'          => "キャンセル",
+    'SAVE'           => "保存"
+);
+
+
+// Language used to send message to new members - intro message
+$LANG_MSG05 = array (
+    'subject'     => "Welcome to my site!",
+    'message'     => "Thank you for joining portalparts.com. I hope that you enjoy being a member :) If you have any questions, please search the FAQ or forum first.<p />Regards,<br><i>Site Administrator</i>"
+);
